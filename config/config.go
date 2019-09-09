600f47d5250487e313b11ed59c21e53b232e93b0
ss-xiong <xiongsongsong.xss@gmail.com>
2019-09-09 22:25:04 +0800

    some change

--- config/config.go
+++ config/config.go
@@ -2,15 +2,49 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
-	"os/user"
+
+	"gopkg.in/yaml.v2"
 )
 
+// AppKey qiniu appkey
+type AppKey struct {
+	AccessKey string `yaml:"AccessKey"`
+	SecretKey string `yaml:"SecretKey"`
+}
+
+// Bucket 需要上传的 Bucket 名字
+type Bucket struct {
+	Name  string `yaml:"Name"`
+	Domin string `yaml:"Domin"`
+}
+
+// AppConfig qiniu app
+type AppConfig struct {
+	AppKey AppKey `yaml:"AppKey"`
+	Bucket Bucket `yaml:"Bucket"`
+}
+
 func main() {
-	user, err := user.Current()
+	// user, err := user.Current()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println(user.HomeDir)
+	var appConfig AppConfig
+
+	// Read config file
+	configFile, err := ioutil.ReadFile("./image4qiniu.yaml")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(user.HomeDir)
+	err = yaml.Unmarshal(configFile, &appConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(appConfig)
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/qiniu/api.v7 v7.2.5+incompatible
 	github.com/qiniu/x v7.0.8+incompatible // indirect
 	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297 // indirect
+	gopkg.in/yaml.v2 v2.2.2
 	qiniupkg.com/x v7.0.8+incompatible // indirect
 )
